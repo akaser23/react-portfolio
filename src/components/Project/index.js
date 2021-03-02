1486a1b7d72da5f2a5decb1a61142aa9ef7121b2
Anna Kaser <kaserann96@gmail.com>
2021-03-01 20:39:40 -0700

    portfolio cards and css

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,35 +1,145 @@
 import React from 'react';
 import githubRed from "../../assets/img/github-red.png";
-// import Card from 'react-bootstrap/Button';
-// import ListGroupItem from 'react-bootstrap/Button';
-// import ListGroup from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ListGroupItem from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/Card';
+import MoodImg from '../../assets/img/Mood Music _ The Weather Based Mood Selector.gif';
+import DinoImg from '../../assets/img/Dinostore.gif';
+import HolidayImg from '../../assets/img/holiday-planner-screen.png';
+import BudgetImg from '../../assets/img/budget-tracker.jpg';
+import BuddyImg from '../../assets/img/Run Buddy.gif';
+import PizzaImg from '../../assets/img/Pizza-Hunt.gif';
 
 function Project() {
     return (
         <div className="flex-row">
-            {/* <div>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <div>
+                <Card style={{ width: '23rem', border: '5px solid #702632', margin: '20px' }}>
+                    <Card.Img variant="top" src={MoodImg} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <Card.Title style={{ textAlign: 'center', fontWeight: '700', fontSize: '1.5rem' }}>Mood Music</Card.Title>
+                        <Card.Text>Web API application that provides the user a playlist and drink recipe depending on the weather.</Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush" >
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>Javascript</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>Web API</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>HTML, CSS</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body style={{ textAlign: 'center' }}>
+                        <Card.Link href="https://akaser23.github.io/music-mood/" target="_blank" rel="noopener noreferrer" style={{ color: '#702632'}}>Visit Site</Card.Link>
+                        <Card.Link href="https://github.com/akaser23/music-mood" target="_blank" rel="noopener noreferrer">
+                            <img className="github-red" src={githubRed} alt="github logo red" />
+                        </Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Card style={{ width: '23rem', border: '5px solid #702632', margin: '20px' }}>
+                    <Card.Img variant="top" src={DinoImg} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center', fontWeight: '700', fontSize: '1.5rem' }}>Dino Store</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            Full-Stack e-commerce application built in React with advanced CSS.
                     </Card.Text>
                     </Card.Body>
                     <ListGroup className="list-group-flush">
-                        <ListGroupItem>Cras justo odio</ListGroupItem>
-                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>React</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>HTML, CSS, Javascript</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>MongoDB</ListGroupItem>
                     </ListGroup>
-                    <Card.Body>
-                        <Card.Link href="#">Card Link</Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
+                    <Card.Body style={{ textAlign: 'center' }}>
+                        <Card.Link href="https://ancient-lake-97986.herokuapp.com/" target="_blank" rel="noopener noreferrer" style={{ color: '#702632'}}>Visit Site</Card.Link>
+                        <Card.Link href="https://github.com/akaser23/dino-store" target="_blank" rel="noopener noreferrer">
+                            <img className="github-red" src={githubRed} alt="github logo red" />
+                        </Card.Link>
                     </Card.Body>
                 </Card>
-            </div> */}
+            </div>
+            <div>
+                <Card style={{ width: '23rem', border: '5px solid #702632', margin: '20px' }}>
+                    <Card.Img variant="top" src={HolidayImg} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center', fontWeight: '700', fontSize: '1.5rem' }}>Holiday Planner</Card.Title>
+                        <Card.Text>Full-Stack application that helps in planning for the holidays.</Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>HTML, CSS, Handlebars.js</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>JQuery, Express.js</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>mySQL, Seqeulize</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body style={{ textAlign: 'center' }}>
+                        <Card.Link href="https://desolate-inlet-16467.herokuapp.com/login" target="_blank" rel="noopener noreferrer" style={{ color: '#702632'}}>Visit Site</Card.Link>
+                        <Card.Link href="https://github.com/akaser23/holiday-planner" target="_blank" rel="noopener noreferrer">
+                            <img className="github-red" src={githubRed} alt="github logo red" />
+                        </Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Card style={{ width: '23rem', border: '5px solid #702632', margin: '20px' }}>
+                    <Card.Img variant="top" src={BudgetImg} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center', fontWeight: '700', fontSize: '1.5rem' }}>Budget Tracker</Card.Title>
+                        <Card.Text>Application that helps to plan expenses and saves all data even when you are offline. </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>HTML, CSS</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>Javascript, Express.js</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>MongoDB, Mongoose</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body style={{ textAlign: 'center' }}>
+                        <Card.Link href="https://damp-reaches-34129.herokuapp.com/" target="_blank" rel="noopener noreferrer" style={{ color: '#702632'}}>Visit Site</Card.Link>
+                        <Card.Link href="https://github.com/akaser23/budget-tracker" target="_blank" rel="noopener noreferrer">
+                            <img className="github-red" src={githubRed} alt="github logo red" />
+                        </Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Card style={{ width: '23rem', border: '5px solid #702632', margin: '20px' }}>
+                    <Card.Img variant="top" src={PizzaImg} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center', fontWeight: '700', fontSize: '1.5rem' }}>Pizza Hunt</Card.Title>
+                        <Card.Text>
+                            The user can create a login, create a pizza to save to the page and leave comments on other pizzas. 
+                    </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>HTML, CSS</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>Javascript, Express.js</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>MongoDB, Mongoose</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body style={{ textAlign: 'center' }}>
+                        <Card.Link href="https://whispering-hollows-55929.herokuapp.com/" target="_blank" rel="noopener noreferrer" style={{ color: '#702632'}}>Visit Site</Card.Link>
+                        <Card.Link href="https://github.com/akaser23/pizza-hunt" target="_blank" rel="noopener noreferrer">
+                            <img className="github-red" src={githubRed} alt="github logo red" />
+                        </Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Card style={{ width: '23rem', border: '5px solid #702632', margin: '20px' }}>
+                    <Card.Img variant="top" src={BuddyImg} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center', fontWeight: '700', fontSize: '1.5rem' }}>Run Buddy</Card.Title>
+                        <Card.Text>
+                            This site displays information about the buisness "Run Buddy" using HTML and CSS.
+                    </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>HTML</ListGroupItem>
+                        <ListGroupItem style={{ margin: '3px', textAlign: 'center', border: '2px solid #702632' }}>CSS</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body style={{ textAlign: 'center' }}>
+                        <Card.Link href="https://akaser23.github.io/run-buddy/" target="_blank" rel="noopener noreferrer" style={{ color: '#702632'}}>Visit Site</Card.Link>
+                        <Card.Link href="https://github.com/akaser23/run-buddy" target="_blank" rel="noopener noreferrer">
+                            <img className="github-red" src={githubRed} alt="github logo red" />
+                        </Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
 
-            <div title="Mood-Music-Gif" className="img-thumbnail-1 mx-1">
+            {/* <div title="Mood-Music-Gif" className="img-thumbnail-1 mx-1">
                 <div className="project-desc">
                     <a className="project-title" href="https://akaser23.github.io/music-mood/" target="_blank" rel="noopener noreferrer">
                         Mood Music
@@ -88,7 +198,7 @@ function Project() {
                         <img src={githubRed} alt="github logo red" />
                     </a>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
